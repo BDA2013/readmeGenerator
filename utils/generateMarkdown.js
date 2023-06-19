fcb477cecdb1955189b8c222c3d7102f8d282caa
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-06-18 21:47:38 -0400

    lines deleted

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
     case 'MIT':
@@ -11,8 +9,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
     case 'MIT':
@@ -24,8 +20,6 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license, creator) {
   switch (license) {
     case 'MIT':
@@ -72,7 +66,6 @@ function renderLicenseSection(license, creator) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -91,10 +84,6 @@ function generateMarkdown(data) {
   ${renderLicenseBadge(data.license)}
 
   ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  ## Features
-  
-  If your project has a lot of features, list them here.
 `;
 }
 
